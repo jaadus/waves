e105615a2befde5945b9305928c9e75cb5ed831e
Veronica Vega <veronica@veronicavega.io>
2017-01-21 11:32:13 -0800

    Goofy input stuff.

--- impact/lib/game/entities/player.js
+++ impact/lib/game/entities/player.js
@@ -10,15 +10,23 @@ EntityPlayer = EntityNetBase.extend({
 	animSheet: new ig.AnimationSheet( 'media/player_1.png', 16, 16 ),
 	size: {x: 16, y: 16},
 
+	gravityFactor: 14,
+
 	isPlayerOne: true,
 
 	syncRate: 0.25,
 
 	_wmIgnore: false,
 
+	speed: 80,
+
+	jumpHeight: 150,
+
+	maxVel: {x: 200, y: 200},
+
 	init: function( x, y, settings ) {
 		this.addAnim( 'idle', 0, [0], true );
-		this.addAnim( 'run', 0.125, [1,2,3,4], true );
+		this.addAnim( 'run', 0.125, [1,2,3,4], false );
 
 		this.parent( x, y, settings );
 	},
@@ -29,9 +37,24 @@ EntityPlayer = EntityNetBase.extend({
 			return;
 		}
 
-		// TODO: Add movement logic here
+		this.handleInput();
 
 		this.parent();
+	},
+
+	handleInput: function() {
+
+		if(ig.input.pressed('jump') && this.standing) {
+		    this.vel.y = -this.jumpHeight;
+		}
+
+		if(ig.input.state('moveLeft')) {
+		    this.vel.x = -this.speed;
+		} else if(ig.input.state('moveRight')) {
+		    this.vel.x = this.speed;
+		} else {
+			this.vel.x = 0;
+		}
 	},
 
 	processPacket: function(msg) {
--- impact/lib/game/main.js
+++ impact/lib/game/main.js
@@ -26,6 +26,7 @@ MyGame = ig.Game.extend({
 		ig.net.login();
 
 		// Initialize your game here; bind keys etc.
+		this.bindKeys();
 
 		this.loadLevel( LevelTest );
 	},
@@ -75,6 +76,13 @@ MyGame = ig.Game.extend({
 		for( var i = 0; i < this.entities.length; i++ ) {
 			this.entities[i].ready();
 		}
+	},
+
+	bindKeys: function() {
+		ig.input.bind( ig.KEY.UP_ARROW, 'jump' );
+		ig.input.bind( ig.KEY.SPACE, 'jump' );
+		ig.input.bind( ig.KEY.RIGHT_ARROW, 'moveRight');
+		ig.input.bind( ig.KEY.LEFT_ARROW, 'moveLeft');
 	},
 
 	update: function() {
